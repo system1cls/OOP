28a942e9fd40bfe08ec5fba7857c07ccbefb006b
system1cls <d.faranosov@gmail.com>
2024-09-03 22:59:04 +0700

    Решение

--- task_1_1_1/src/main/java/org/example/HeapSort.java
+++ task_1_1_1/src/main/java/org/example/HeapSort.java
@@ -6,83 +6,83 @@ package org.example;
  */
 public class HeapSort {
 
-    /**
-     * Метод меняет местами элементы в массиве.
-     *
-     * @param arr   - массив
-     * @param left  - номер первого элемента в массиве
-     * @param right - номер второго элемента в массиве
-     */
-    void swap(int[] arr, int left, int right) {
-        int temp = arr[left];
-        arr[left] = arr[right];
-        arr[right] = temp;
+  /**
+   * Метод меняет местами элементы в массиве.
+   *
+   * @param arr   - массив
+   * @param left  - номер первого элемента в массиве
+   * @param right - номер второго элемента в массиве
+   */
+  void swap(int[] arr, int left, int right) {
+    int temp = arr[left];
+    arr[left] = arr[right];
+    arr[right] = temp;
 
-    }
+  }
 
 
-    /**
-     * Метод выполняет просеивание на родителе и двух детях.
-     *
-     * @param arr    - массив
-     * @param length - длина массива
-     * @param source - номер элемента для просеивания
-     */
-    void heapify(int[] arr, int length, int source) {
-        int left = source * 2 + 1;
-        int right = source * 2 + 2;
+  /**
+   * Метод выполняет просеивание на родителе и двух детях.
+   *
+   * @param arr    - массив
+   * @param length - длина массива
+   * @param source - номер элемента для просеивания
+   */
+  void heapify(int[] arr, int length, int source) {
+    int left = source * 2 + 1;
+    int right = source * 2 + 2;
 
-        if (left >= length) {
-            return;
-        } else if (right >= length) {
-            if (arr[source] < arr[left]) {
-                swap(arr, source, left);
-            }
-        } else {
-            if (arr[left] >= arr[right]) {
-                if (arr[source] < arr[left]) {
-                    swap(arr, source, left);
-                }
-            } else {
-                if (arr[source] < arr[right]) {
-                    swap(arr, source, right);
-                }
-            }
+    if (left >= length) {
+      return;
+    } else if (right >= length) {
+      if (arr[source] < arr[left]) {
+        swap(arr, source, left);
+      }
+    } else {
+      if (arr[left] >= arr[right]) {
+        if (arr[source] < arr[left]) {
+          swap(arr, source, left);
         }
-    }
-
-    /**
-     * Метод выполняет просеивание.
-     *
-     * @param arr    - массив
-     * @param length - его длина
-     */
-    void heapify_all(int[] arr, int length) {
-        for (int i = length / 2 - 1; i >= 0; i--) {
-            heapify(arr, length, i);
+      } else {
+        if (arr[source] < arr[right]) {
+          swap(arr, source, right);
         }
+      }
+    }
+  }
+
+  /**
+   * Метод выполняет просеивание.
+   *
+   * @param arr    - массив
+   * @param length - его длина
+   */
+  void heapify_all(int[] arr, int length) {
+    for (int i = length / 2 - 1; i >= 0; i--) {
+      heapify(arr, length, i);
+    }
+  }
+
+
+  /**
+   * Метод сортирует массив.
+   *
+   * @param arr - массив
+   */
+  public void heapsort(int[] arr) {
+    int length = arr.length;
+    int n = length;
+    int source;
+
+    heapify_all(arr, length);
+
+    for (int i = 0; i < length - 1; i++) {
+      swap(arr, 0, n - 1);
+      n--;
+      heapify_all(arr, n);
     }
 
 
-    /**
-     * Метод сортирует массив.
-     *
-     * @param arr - массив
-     */
-    public void heapsort(int[] arr) {
-        int length = arr.length;
-        int n = length;
-        int source;
-
-        heapify_all(arr, length);
-
-        for (int i = 0; i < length - 1; i++) {
-            swap(arr, 0, n - 1);
-            n--;
-            heapify_all(arr, n);
-        }
-
-
-    }
+  }
 
 }
--- task_1_1_1/src/main/java/org/example/Main.java
+++ task_1_1_1/src/main/java/org/example/Main.java
@@ -8,18 +8,19 @@ import java.util.Arrays;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    /**
-     * управляющий метод.
-     *
-     * @param args - параметры командной строки
-     */
-    public static void main(String[] args) {
 
-        int[] arr = new int[]{5, 4, 3, 2, 1};
-        HeapSort srt = new HeapSort();
-        srt.heapsort(arr);
-        System.out.println(Arrays.toString(arr));
-    }
+  /**
+   * управляющий метод.
+   *
+   * @param args - параметры командной строки
+   */
+  public static void main(String[] args) {
+
+    int[] arr = new int[]{5, 4, 3, 2, 1};
+    HeapSort srt = new HeapSort();
+    srt.heapsort(arr);
+    System.out.println(Arrays.toString(arr));
+  }
 }
 
 
