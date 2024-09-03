01ac426cfbc1d46560b2a750ab224ecb265cd48c
system1cls <d.faranosov@gmail.com>
2024-09-03 23:05:13 +0700

    Решение

--- task_1_1_1/src/main/java/org/example/Main.java
+++ task_1_1_1/src/main/java/org/example/Main.java
@@ -9,18 +9,18 @@ import java.util.Arrays;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-  /**
-   * управляющий метод.
-   *
-   * @param args - параметры командной строки
-   */
-  public static void main(String[] args) {
+    /**
+     * управляющий метод.
+     *
+     * @param args - параметры командной строки
+     */
+    public static void main(String[] args) {
 
-    int[] arr = new int[]{5, 4, 3, 2, 1};
-    HeapSort srt = new HeapSort();
-    srt.heapsort(arr);
-    System.out.println(Arrays.toString(arr));
-  }
+        int[] arr = new int[]{5, 4, 3, 2, 1};
+        HeapSort srt = new HeapSort();
+        srt.heapsort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 }
 
 
--- task_1_1_1/src/test/java/org/example/HeapSortTest.java
+++ task_1_1_1/src/test/java/org/example/HeapSortTest.java
@@ -6,18 +6,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class HeapSortTest {
 
-  @Test
-  void heapsort() {
-    int[] arr = {5, 4, 3, 2, 1};
-    HeapSort srt = new HeapSort();
-    srt.heapsort(arr);
-    assertEquals(1, arr[0]);
-    assertEquals(5, arr[4]);
+    @Test
+    void heapsort() {
+        int[] arr = {5, 4, 3, 2, 1};
+        HeapSort srt = new HeapSort();
+        srt.heapsort(arr);
+        assertEquals(1, arr[0]);
+        assertEquals(5, arr[4]);
 
-    int[] arr1 = {};
-    srt.heapsort(arr1);
-    assertEquals(arr1.length, 0);
+        int[] arr1 = {};
+        srt.heapsort(arr1);
+        assertEquals(arr1.length, 0);
 
 
-  }
+    }
 }
