d9ec56065bc4147504337c5f55e7dfc04d6a4f7a
system1cls <d.faranosov@gmail.com>
2024-09-04 07:52:45 +0700

    Решение 2.0

---
+++ task_1_1_2/src/main/java/org/example/HeapSort.java
@@ -0,0 +1,110 @@
+package org.example;
+
+
+/**
+ * Отдельный класс для сортировки.
+ */
+public class HeapSort {
+
+    /**
+     * Метод меняет местами элементы в массиве.
+     *
+     * @param arr   - массив
+     * @param left  - номер первого элемента в массиве
+     * @param right - номер второго элемента в массиве
+     */
+    void swap(int[] arr, int left, int right) {
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+
+    }
+
+
+    /**
+     * Метод выполняет просеивание на родителе и двух детях.
+     *
+     * @param arr    - массив
+     * @param length - длина массива
+     * @param source - номер элемента для просеивания
+     */
+    int heapify(int[] arr, int length, int source) {
+        int left = source * 2 + 1;
+        int right = source * 2 + 2;
+
+        if (left >= length) {
+            return 0;
+        } else if (right >= length) {
+            if (arr[source] < arr[left]) {
+                swap(arr, source, left);
+                return 1;
+            }
+        } else {
+            if (arr[left] >= arr[right]) {
+                if (arr[source] < arr[left]) {
+                    swap(arr, source, left);
+                    return 1;
+                }
+            } else {
+                if (arr[source] < arr[right]) {
+                    swap(arr, source, right);
+                    return 2;
+                }
+            }
+        }
+        return 0;
+    }
+
+
+    /**
+     * Выполняет просеивание по одной ветке вниз.
+     *
+     * @param arr    - массив
+     * @param length - длина массива
+     * @param source - номер родителя для просеивания
+     */
+    public void heapify_down(int[] arr, int length, int source) {
+        int id = 0;
+        id = heapify(arr, length, source);
+        if (id != 0) heapify_down(arr, length, source * 2 + id);
+    }
+
+
+    /**
+     * Метод выполняет просеивание.
+     *
+     * @param arr    - массив
+     * @param length - его длина
+     */
+    void heapify_all(int[] arr, int length) {
+        int id = 0;
+        for (int i = length / 2 - 1; i >= 0; i--) {
+            id = heapify(arr, length, i);
+
+            if (id >= 0) heapify_down(arr, length, i * 2 + id);
+        }
+    }
+
+
+    /**
+     * Метод сортирует массив.
+     *
+     * @param arr - массив
+     */
+    public void heapsort(int[] arr) {
+        int length = arr.length;
+        int n = length;
+        int source;
+
+        heapify_all(arr, length);
+
+        for (int i = 0; i < length - 1; i++) {
+            swap(arr, 0, n - 1);
+            n--;
+            heapify_down(arr, n, 0);
+        }
+
+
+    }
+
+}
---
+++ task_1_1_2/src/test/java/org/example/HeapSortTest.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeapSortTest {
+
+    @Test
+    void heapsort() {
+        int[] arr = {5, 4, 3, 2, 1};
+        HeapSort srt = new HeapSort();
+        srt.heapsort(arr);
+        assertEquals(1, arr[0]);
+        assertEquals(5, arr[4]);
+
+        int[] arr1 = {};
+        srt.heapsort(arr1);
+        assertEquals(arr1.length, 0);
+    }
+}
