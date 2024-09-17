419acb371595948b5cbb11dba99a8d25dde0a4e6
system1cls <system1cls@users.noreply.github.com>
2024-09-17 06:08:34 +0000

    Deploying to gh-pages from @ system1cls/OOP@e3ea58221f471d28f32044e05377e080991a6840 🚀

--- task_1_1_1/type-search-index.js
+++ task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"HeapSort"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"HeapSort"},{"p":"org.example","l":"Main"}]
