758c72e4d1ac889e69ba3828346b58e42147c422
Dmitrii Faranosov <d.faranosov@g.nsu.ru>
2024-10-01 09:07:06 +0700

    task_1_1_3

--- task_1_1_3/src/test/java/org/example/evalTest.java
+++ task_1_1_3/src/test/java/org/example/evalTest.java
@@ -33,7 +33,7 @@ class evalTest {
     @Test
     void evalTest5(){
         Expression ex = Expression.make_expression("1-2-3-4-5-6-7-8-9");
-        assertEquals(30, ex.eval("x=2;y=0; z = 6"));
+        assertEquals(-43, ex.eval("x=2;y=0; z = 6"));
     }
 
 }
