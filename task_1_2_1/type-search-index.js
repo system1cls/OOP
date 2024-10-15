3f9879f682ee3b727a9a11d91d6c65e42becf181
system1cls <system1cls@users.noreply.github.com>
2024-10-15 01:55:08 +0000

    Deploying to gh-pages from @ system1cls/OOP@8a1ac53c48ff4f9d84aec3dd907900be745d9ae2 🚀

---
+++ task_1_2_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"org.example","l":"AdjMatrix"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"EdgeMatrix"},{"p":"org.example","l":"Graph"},{"p":"org.example","l":"ListAdj"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"Vert"}]
