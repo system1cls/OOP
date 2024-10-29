d0bed800b465562bcad81fbb31adf819724aaec7
system1cls <system1cls@users.noreply.github.com>
2024-10-29 02:21:14 +0000

    Deploying to gh-pages from @ system1cls/OOP@c04ce51d056a74cb3655b5b8495f497eeaa7e0c7 🚀

---
+++ task_1_2_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"HashIterator"},{"p":"org.example","l":"HashTable"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"Pair"},{"p":"org.example","l":"SubArray"}]
